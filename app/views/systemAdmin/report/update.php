3a716919f51af52eecc07d136bfb0c010f6d74f3
Arif <dopymonster@gmail.com>
2026-05-07 14:15:46 +0800

    add file

---
+++ app/views/systemAdmin/report/update.php
@@ -0,0 +1,17 @@
+<?php include(__DIR__ . "/../../header.php");
+
+// authorize(["user"]);
+?>
+
+<link rel="stylesheet" href="<?= BASE_URL ?>/user/style/dashboard.css">
+
+<main>
+
+</main>
+
+<script src="<?= BASE_URL ?>/user/script/dashboard.js"></script>
+<script>
+
+</script>
+
+<?php include(__DIR__ . "/../../footer.php"); ?>
